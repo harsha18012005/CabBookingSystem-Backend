540fc776d0c5a45455c110526459dd2d015f0156
harshasekhar <harshaloyal9@gmail.com>
2025-11-03 19:05:42 +0530

    Refactor main method formatting in CabbookingApplication

--- CabbookingApplication.java
+++ CabbookingApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CabbookingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CabbookingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CabbookingApplication.class, args);
+    }
 }
